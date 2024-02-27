419d56353d41a1035ca34a51f9acce361034d615
Esther Smeke <esthersmeke@gmail.com>
2024-02-26 19:52:50 -0600

    Fixed ClearCart

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -2,28 +2,36 @@ import { createContext, useState } from "react";
 
 export const CartContext = createContext({
   cart: [],
+  totalQuantity: 0, // Agregar totalQuantity al contexto
+  clearCart: () => {}, // Agregar clearCart al contexto
 });
 
 export const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
+  const [totalQuantity, setTotalQuantity] = useState(0); // Estado para mantener el totalQuantity
 
   console.log(cart);
 
   const addItem = (item, quantity) => {
     if (!isInCart(item.id)) {
       setCart((prev) => [...prev, { ...item, quantity }]);
+      setTotalQuantity((prevTotal) => prevTotal + quantity); // Actualiza totalQuantity
+      console.log("Cart Qty: " + totalQuantity);
     } else {
-      console.error("The product has already been added");
+      alert("The product has already been added");
+      // Màs bien lo que tendrìa que hacer es si ya hay, sumar la cantidad a los que ya estàn en el carrito
     }
   };
 
   const removeItem = (itemId) => {
     const cartUpdated = cart.filter((prod) => prod.id !== itemId);
     setCart(cartUpdated);
+    console.log("Cart Qty: " + totalQuantity);
   };
 
   const clearCart = () => {
     setCart([]);
+    setTotalQuantity(0); // Reinicia totalQuantity
   };
 
   const isInCart = (itemId) => {
@@ -31,7 +39,9 @@ export const CartProvider = ({ children }) => {
   };
 
   return (
-    <CartContext.Provider value={{ cart, addItem, removeItem, clearCart }}>
+    <CartContext.Provider
+      value={{ cart, totalQuantity, addItem, removeItem, clearCart }}
+    >
       {children}
     </CartContext.Provider>
   );
