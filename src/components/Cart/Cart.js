79e05f843c2899f1a75c088cd65f579c490cc887
Esther Smeke <esthersmeke@gmail.com>
2024-02-28 01:08:44 -0600

    Fix cart checkout

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,12 +1,42 @@
 import "./Cart.css";
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { CartContext } from "../../context/CartContext";
 import CartItem from "../CartItem/CartItem";
 import { Link } from "react-router-dom";
+import { getFirestore, addDoc, collection } from "firebase/firestore";
 
 const Cart = () => {
   const { cart, clearCart, totalQuantity, totalAmount } =
     useContext(CartContext);
+
+  function sendOrder() {
+    console.log("Items: ", cart);
+
+    const selectedItems = cart.map((item) => ({
+      title: item.title,
+      price: item.price,
+    }));
+
+    const order = {
+      buyer: {
+        name: "María",
+        phone: "555555",
+        email: "me@mail.com",
+      },
+      items: selectedItems,
+      total: totalAmount,
+    };
+
+    const db = getFirestore();
+
+    const orderCollection = collection(db, "orders");
+
+    console.log(order);
+
+    addDoc(orderCollection, order).then(({ id }) =>
+      console.log("ID de la orden: ", id)
+    );
+  }
 
   if (totalQuantity === 0) {
     return (
@@ -22,15 +52,15 @@ const Cart = () => {
   return (
     <div>
       {cart.map((p) => (
-        <CartItem key={p.itemId} {...p} />
+        <CartItem key={p.title} {...p} />
       ))}
       <h3>Total: ${totalAmount}</h3>
       <button onClick={() => clearCart()} className="Button">
         Clear Cart
       </button>
-      <Link to="/checkout" className="Option">
+      <button className="btn-pay" onClick={sendOrder}>
         Checkout
-      </Link>
+      </button>
     </div>
   );
 };
